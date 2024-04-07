b6cb69210a03157bdbecac8dba8b72574c642892
damien <contact.damiendiaz@gmail.com>
2024-04-07 19:06:47 +0200

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import { cn } from "@/lib/utils";
+import { GeistSans } from "geist/font/sans";
+import type { Metadata } from "next";
+import { Anek_Telugu } from "next/font/google";
+import "./globals.css";
+
+const AnekTelugu = Anek_Telugu({ 
+  subsets: ["latin"],
+  variable: "--font-caption",
+});
+
+export const metadata: Metadata = {
+  title: "Damien Diaz . Sofware Engineer",
+  description: "Online creator and educator about React, Tailwind and Symfony",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="h-full">
+      <body className={cn(GeistSans.variable, AnekTelugu, "font-sans h-full bg-background text-foreground")}>{children}</body>
+    </html>
+  );
+}
