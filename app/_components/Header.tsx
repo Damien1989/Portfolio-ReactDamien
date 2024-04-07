c8b4da64cd1de4f521a20fdb5a24ebafb3304e2b
damien <contact.damiendiaz@gmail.com>
2024-04-07 19:27:40 +0200

    My github

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,21 +1,26 @@
-import { Button } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 import { Section } from "./Section";
+import Link from "next/link";
 import { GithubIcon } from "./icons/GithubIcon";
 
 
 export const Header = () => {
     return (
-        <header className="sticky top-0">
+        <header className="sticky top-0 py-4">
             <Section className="flex items-baseline">
-                <h1 className="text-lg font-bold text-primary-foreground">Damien.com
+                <h1 className="text-lg font-bold text-primary">Damien.com
                 </h1>
                 <div className="flex-1" />
                 <ul>
-                    <Button className="size-6">
-                        <GithubIcon size={16} className="text-foreground" />
-                    </Button>
+                    <Link 
+                    href="https://github.com/Damien1989"
+                    className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
+                    >
+                        <GithubIcon size={19} className="text-foreground" />
+                    </Link>
                 </ul>
             </Section>
         </header>
-    )
-}
+    );
+};
