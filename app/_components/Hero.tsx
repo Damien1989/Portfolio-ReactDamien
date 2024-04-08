d8476bef500cfd4a54e5bd16450beddaf0169006
damien <contact.damiendiaz@gmail.com>
2024-04-08 14:57:39 +0200

    photo presentation portfolio and first creation symfony

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -1,18 +1,31 @@
-import React from 'react';
+import { ComponentPropsWithoutRef } from "react";
 import { Section } from "./Section";
+import { cn } from "@/lib/utils";
+import { SymfonyLogo } from "./icons/SymfonyLogo";
+
+const Code = ({className,... props}: ComponentPropsWithoutRef<"span">) => {
+    return (
+    <span className={cn("bg-accent/30 font-mono border border-accent p-1 text-primary rounded-sm",className)}
+    {...props} 
+    />
+  );
+};
 
 export const Hero = () => {
-    return <Section className="flex max-lg:flex-col items-start">
+    return <Section className="flex max-md:flex-col items-start">
 <div className="flex-[2]">
-    <h2>Damien Diaz</h2>
-    <h3>Sofware Developer and creator</h3>
-    <p>
-        Lorem Ipsum
+    <h2 className="font-caption font-bold text-5xl text-primary">Damien Diaz</h2>
+    <h3 className="text-3xl font-caption">Sofware Developer and creator</h3>
+    <p className="text-base">
+        J'aime travailler sur {" "}
+        <Code className="inline-flex items-center gap-1">
+           <SymfonyLogo size={1} className="inline" /> Symfony
+        </Code>
     </p>
 </div>
 <div className="flex-1">
 <img src="/images/damien2.jpeg"
-    className="w-full auto"
+    className="w-full h-auto max-w-xs"
     alt="photo Damien"
     ></img>
 </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import type { Metadata } from "next";
 import { Anek_Telugu } from "next/font/google";
 import "./globals.css";
@@ -21,7 +22,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full">
-      <body className={cn(GeistSans.variable, AnekTelugu, "font-sans h-full bg-background text-foreground")}>{children}</body>
+      <body className={cn(
+      GeistSans.variable, 
+      GeistMono.variable,
+      AnekTelugu.variable, 
+      "font-sans h-full bg-background text-foreground"
+      )}
+      >
+      {children}
+      </body>
     </html>
   );
 }
