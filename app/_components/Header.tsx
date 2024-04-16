525228029b66e8de6ecc2c0c38b046200f3d6dfe
damien <contact.damiendiaz@gmail.com>
2024-04-16 16:27:09 +0200

    alignement header

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -7,20 +7,18 @@ import { LinkedinIcon } from "./icons/LinkedinIcon";
 
 
 export const Header = () => {
-    return (
+   return (
         <header className="sticky top-0 py-4">
-            <Section className="d-flex align-items-center justify-content-between">
-                <h1 className="text-lg font-bold text-primary flex-grow-1">Mon portfolio
-                </h1>
-                <ul className="list-unstyled m-0 flex items-center gap-2">
-                <li className="ml-auto">
-                    <Link 
-                    href="https://github.com/Damien1989"
+            <Section className="flex items-center justify-content-between">
+            <div className="flex items-center">
+                <h1 className="text-lg font-bold text-primary flex-grow-1">Mon portfolio</h1>
+                <ul className="list-unstyled m-0 flex items-center gap-2 ml-auto">
+                    <Link href="https://github.com/Damien1989"
                     className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
                     >
                         <GithubIcon size={16} className="text-foreground" />
                     </Link>
-                    </li>
+                    
                     <Link 
                     href="https://www.linkedin.com/in/damien-diaz-2453781b2/"
                     className={cn(buttonVariants({ variant: "outline" }), "size-6 p-0")}
@@ -28,6 +26,7 @@ export const Header = () => {
                         <LinkedinIcon size={16} className="text-foreground" />
                     </Link>
                 </ul>
+                </div>
             </Section>
         </header>
     );
