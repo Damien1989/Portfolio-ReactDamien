2792784c11f6468866c567eae948032724751496
damien <contact.damiendiaz@gmail.com>
2024-04-19 16:00:31 +0200

    skills space

--- app/_components/Skills.tsx
+++ app/_components/Skills.tsx
@@ -7,33 +7,33 @@ import { SymfonyLogo } from "./icons/SymfonyLogo";
 
 
 export const Skills = () => {
-    return <Section className="flex flex-col items-start gap-4">
+    return <Section className="flex flex-col items-start gap-5">
         <Badge variant={"outline"}>Skills</Badge>
     <h2 className="pb-2 text-3xl mt-4 font-semibold tracking-tight first:mt-0 mb-4">
         J'aime travailler sur ...
         </h2>
-              <div className="flex flex-col gap-2 flex-1">
+              <div className="flex flex-col gap-4 flex-1">
                 <ReactLogo size={42} 
                  className="animate-spin" 
                  style={{ animationDuration : "10s",
                   }} />
               <h3 className="text-2xl font-semibold tracking-tight">React</h3>
-              <p className="text-sm text-muted-foreground">En cours d'apprentissage de React mais aussi de <Code>Next.js</Code>
+              <p className="text-sm text-muted-foreground gap-3">En cours d'apprentissage de React mais aussi de <Code>Next.js</Code>
               </p>
           </div>
 
           <div className="flex max-md:flex-col gap-4">
-              <div className="flex flex-col gap-2">
+              <div className="flex flex-col gap-3 mt-4">
                 <OpenAILogo size={42} 
                  />
-              <h3 className="text-2xl font-semibold tracking-tight">Open AI</h3>
+              <h3 className="text-2xl font-semibold tracking-tight mt-4">Open AI</h3>
               <p className="text-sm text-muted-foreground"><Code>Chat GPT</Code> est mon outil de travail favori en matière d'IA. Je consulte les nouveautés sur <Code>Futurepedia</Code> .
               </p>
           </div>
           </div>
 
           <div className="flex max-md:flex-col gap-4">
-              <div className="flex flex-col gap-2">
+              <div className="flex flex-col gap-2 mt-4">
                 <SymfonyLogo size={42} 
                   />
               <h3 className="text-2xl font-semibold tracking-tight">Symfony</h3>
